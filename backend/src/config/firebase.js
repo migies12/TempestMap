09821979ad218f16d5870c09020191a0fa1ae750
armaan-braich <armaanbraich@hotmail.com>
2025-04-02 00:10:35 -0700

    testing notification fix

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -2,7 +2,8 @@ const { initializeApp, applicationDefault } = require('firebase-admin/app');
 const { getMessaging } = require('firebase-admin/messaging');
 
 const firebaseApp = initializeApp({
-  credential: applicationDefault()
+  credential: applicationDefault(),
+  projectId: 'tempestmap-f0234'
 });
 
 module.exports = { firebaseApp, getMessaging };
